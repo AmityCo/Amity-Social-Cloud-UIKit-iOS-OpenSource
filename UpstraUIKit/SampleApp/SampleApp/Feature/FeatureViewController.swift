afdd85c9bf392aa06f9eb5f68cd1fb40287e1513
Thavorn Kamolsin <ka.thavorn@gmail.com>
2021-11-16 16:39:35 +0700

    2.6.10

--- UpstraUIKit/SampleApp/SampleApp/Feature/FeatureViewController.swift
+++ UpstraUIKit/SampleApp/SampleApp/Feature/FeatureViewController.swift
@@ -32,7 +32,7 @@ class FeatureViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "[UIKit 2.6.1] [TrueID Build 2.6.1]"
+        title = "[UIKit 2.6.1] [TrueID Build 2.6.10]"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
         tableView.delegate = self
         tableView.dataSource = self
