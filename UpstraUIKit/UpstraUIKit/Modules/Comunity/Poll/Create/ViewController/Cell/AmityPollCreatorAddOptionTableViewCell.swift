85b7dd56a6f3f0e4ef8adc886786a38f09a67ca0
Thavorn Kamolsin <ka.thavorn@gmail.com>
2021-12-21 15:50:38 +0700

    Hidden Button Add options at poll in post

--- UpstraUIKit/UpstraUIKit/Modules/Comunity/Poll/Create/ViewController/Cell/AmityPollCreatorAddOptionTableViewCell.swift
+++ UpstraUIKit/UpstraUIKit/Modules/Comunity/Poll/Create/ViewController/Cell/AmityPollCreatorAddOptionTableViewCell.swift
@@ -38,17 +38,18 @@ final class AmityPollCreatorAddOptionTableViewCell: UITableViewCell, Nibbable, A
     }
     
     func updateAddAnswerOptionButton(isMaxAnswer: Bool) {
-        if isMaxAnswer {
-            addAnswerOptionButton.setTitleColor(AmityColorSet.base.blend(.shade3), for: .normal)
-            addAnswerOptionButton.titleLabel?.font = AmityFontSet.bodyBold
-            addAnswerOptionButton.setImage(AmityIconSet.iconPollOptionAdd, for: .normal)
-            addAnswerOptionButton.tintColor = AmityColorSet.base.blend(.shade3)
-        } else {
-            addAnswerOptionButton.setTitleColor(AmityColorSet.base, for: .normal)
-            addAnswerOptionButton.titleLabel?.font = AmityFontSet.bodyBold
-            addAnswerOptionButton.setImage(AmityIconSet.iconPollOptionAdd, for: .normal)
-            addAnswerOptionButton.tintColor = AmityColorSet.base
-        }
+        addAnswerOptionButton.isHidden = isMaxAnswer
+//        if isMaxAnswer {
+//            addAnswerOptionButton.setTitleColor(AmityColorSet.base.blend(.shade3), for: .normal)
+//            addAnswerOptionButton.titleLabel?.font = AmityFontSet.bodyBold
+//            addAnswerOptionButton.setImage(AmityIconSet.iconPollOptionAdd, for: .normal)
+//            addAnswerOptionButton.tintColor = AmityColorSet.base.blend(.shade3)
+//        } else {
+//            addAnswerOptionButton.setTitleColor(AmityColorSet.base, for: .normal)
+//            addAnswerOptionButton.titleLabel?.font = AmityFontSet.bodyBold
+//            addAnswerOptionButton.setImage(AmityIconSet.iconPollOptionAdd, for: .normal)
+//            addAnswerOptionButton.tintColor = AmityColorSet.base
+//        }
     }
     
     // MARK: - Action
