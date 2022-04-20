e8ecd66bc9c09b230c8058335c2faafc7da1afcc
MonoAmity <patcharapon@amity.co>
2022-04-20 16:41:00 +0700

    Full Header

---
+++ UpstraUIKit/UpstraUIKit/Modules/Comunity/Home/AmityCommunityHomePageHeaderViewControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AmityCommunityHomePageHeaderViewControllerViewController.swift
+//  AmityUIKit
+//
+//  Created by Mono TheForestcat on 20/4/2565 BE.
+//  Copyright © 2565 BE Amity. All rights reserved.
+//
+
+import UIKit
+
+class AmityCommunityHomePageHeaderViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
