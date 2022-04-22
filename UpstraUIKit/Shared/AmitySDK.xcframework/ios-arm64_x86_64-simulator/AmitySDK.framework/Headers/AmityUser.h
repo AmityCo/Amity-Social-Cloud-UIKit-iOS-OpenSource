59fb29751a55338642229d887b54ac4b8d37545a
Nontapat Siengsanor <nontapat@amity.co>
2022-04-22 10:39:56 +0700

    Update AmityUIKit v2.15.0

--- UpstraUIKit/Shared/AmitySDK.xcframework/ios-arm64_x86_64-simulator/AmitySDK.framework/Headers/AmityUser.h
+++ UpstraUIKit/Shared/AmitySDK.xcframework/ios-arm64_x86_64-simulator/AmitySDK.framework/Headers/AmityUser.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57b94062b42dcd09fa6d5387227e9cad90b82b390b1c620069b3fbcd7a31b8cf
-size 2259
+oid sha256:f7f83218efd3f8243720ca1ad31d5abbdbcd3fad53e79cad7b577e51873f57f6
+size 2367
