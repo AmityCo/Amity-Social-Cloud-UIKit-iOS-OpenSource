722e01d0c9676a3d923c85e84229eeef1d961d40
MonoAmity <patcharapon@amity.co>
2022-09-15 14:49:44 +0700

    Fix: Streamer textview float over keyboard

--- UpstraUIKit/AmityUIKitLiveStream/AmityUIKitLiveStream/New Group/LiveStreamBroadcastViewController.swift
+++ UpstraUIKit/AmityUIKitLiveStream/AmityUIKitLiveStream/New Group/LiveStreamBroadcastViewController.swift
@@ -797,8 +797,9 @@ extension LiveStreamBroadcastViewController {
 extension LiveStreamBroadcastViewController {
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let safeAreaBottom = view.safeAreaInsets.bottom
             if streamingViewBottomConstraint.constant == 0.0 {
-                streamingViewBottomConstraint.constant += keyboardSize.height
+                streamingViewBottomConstraint.constant += (keyboardSize.height - safeAreaBottom)
                 UIView.animate(withDuration: 0.5) {
                     self.view.layoutIfNeeded()
                 }
@@ -807,9 +808,6 @@ extension LiveStreamBroadcastViewController {
     }
 
     @objc func keyboardWillHide(notification: NSNotification) {
-//        if self.view.frame.origin.y != 0 {
-//            self.view.frame.origin.y = 0
-//        }
         if streamingViewBottomConstraint.constant != 0.0 {
             streamingViewBottomConstraint.constant = 0.0
             UIView.animate(withDuration: 0.5) {
